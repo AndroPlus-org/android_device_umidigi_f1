585eeea44e121a31153968a771ffd1ea67bba5b8
AndroPlus <mail@andro.plus>
2019-02-09 12:59:59 +0900

    Fix build

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,7 +28,7 @@ TARGET_CPU_VARIANT := cortex-a53
 TARGET_CPU_SMP := true
 
 TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_ARCH_VARIANT := armv8-a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
@@ -77,3 +77,4 @@ TW_MTP_DEVICE := /dev/mtp_usb
 TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 TW_THEME := portrait_hdpi
+TW_USE_TOOLBOX := true
